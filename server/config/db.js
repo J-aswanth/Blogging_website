4e62a989b8937cc45c8efe56c867d0a656bee1fa
Jaswanth Malla <128897545+J-aswanth@users.noreply.github.com>
2024-07-01 23:46:04 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
-const connectDB = async () => {
-  
-  try {
-    mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Database Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-  }
+import mongoose from 'mongoose';
 
+
+const connectDB = async () =>{
+
+    try {
+
+        mongoose.set('strictQuery',false);
+        const conn = await mongoose.connect(process.env.MONGODB_URL);
+        console.log(`Database connected: ${conn.connection.host}`);
+        
+    }catch (error){
+        console.log(error);
+    }
 }
 
-module.exports = connectDB;
+export default connectDB;
