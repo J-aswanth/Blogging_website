d62f80caae880f2d7d878f864045a3dcefc31d85
Jaswanth Malla <128897545+J-aswanth@users.noreply.github.com>
2024-07-01 23:47:57 +0530

    done

--- server/models/User.js
+++ server/models/User.js
@@ -1,16 +1,18 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-  }
-});
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User',UserSchema);
+
+export default User;
