99a62473e2a2525bc8e651686e4168ce7769a6ae
Jaswanth Malla <128897545+J-aswanth@users.noreply.github.com>
2024-07-01 23:47:31 +0530

    done

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,23 +1,24 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-const Schema = mongoose.Schema;
-const PostSchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  body: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+})
 
-module.exports = mongoose.model('Post', PostSchema);
+const Post = mongoose.model('Post',PostSchema);
+
+export default Post;
