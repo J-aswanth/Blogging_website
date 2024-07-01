1ac43d04b00bc3183e05552d8967fe6db74060d8
Jaswanth Malla <128897545+J-aswanth@users.noreply.github.com>
2024-07-02 00:04:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,68 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const express = require('express');
-const expressLayout = require('express-ejs-layouts');
-const methodOverride = require('method-override');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+import dotenv from "dotenv";
+dotenv.config();
 
-const connectDB = require('./server/config/db');
-const { isActiveRoute } = require('./server/helpers/routeHelpers');
+import express from "express";
+import expressEjsLayouts from "express-ejs-layouts";
+
+import methodOverride from "method-override";
+import cookieParser from "cookie-parser";
+import MongoStore from "connect-mongo";
+import session from "express-session";
+
+import router from './server/routes/main.js';
+import connectDB from './server/config/db.js';
+import routers from './server/routes/admin.js';
+import isActiveRoute from "./server/helpers/routeHelpers.js";
+
+
+
+
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-  
-// Connect to DB
+const PORT = 8000 ||  process.env.PORT;
+
+// Invoke connectDB function
 connectDB();
 
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
 app.use(cookieParser());
+
 app.use(methodOverride('_method'));
 
 app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI
-  }),
-  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URL
+    }),
+    
+//cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+
 }));
+
+
 
 app.use(express.static('public'));
 
-// Templating Engine
-app.use(expressLayout);
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+app.use(expressEjsLayouts);
+app.set('layout','./layout/main');
+app.set('view engine','ejs');
 
 
-app.locals.isActiveRoute = isActiveRoute; 
+app.locals.isActiveRoute = isActiveRoute;
+
+app.use('/',router);
+app.use('/',routers);
 
 
-app.use('/', require('./server/routes/main'));
-app.use('/', require('./server/routes/admin'));
 
-app.listen(PORT, ()=> {
-  console.log(`App listening on port ${PORT}`);
-});
+
+app.listen(PORT,()=>{
+    console.log('App lisssssssss');
+})
